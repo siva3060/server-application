54c489befb55301f531a385bc3a27b5190536c20
siva3060 <siva3060@gmail.com>
2021-02-05 01:33:14 +0530

    Update POM & Data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -40,3 +40,11 @@ values
  (8,
  'aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a4006020002440001784400017978704023000000000000404bfa1cac083127', now(), now(), false, 'ONLINE',
 'driver08pw', 'driver08');
+
+-- Create 3 Cars
+insert into car (id, date_created, license_plate, seat_count, is_convertible, rating, engine_type, make, model, is_avaliable, booked_by)
+values (1, now(), 'KA51E342', 4, false, 4.2,'ELECTRIC','Tesla','Model X', true,0);
+insert into car (id, date_created, license_plate, seat_count, is_convertible, rating, engine_type, make, model, is_avaliable, booked_by)
+values (2, now(), 'AP01E23', 2, true, 4.5,'HYBRID','FORD','e-pickup', true,0);
+insert into car (id, date_created, license_plate, seat_count, is_convertible, rating, engine_type, make, model, is_avaliable, booked_by)
+values (3, now(), 'KL01D898', 8, false, 3.2,'GAS','Honda','Accord', true,0);
